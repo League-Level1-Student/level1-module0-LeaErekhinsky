8d0f05ed0a05934f612b35a793f4f610840a9e18
league <maddoxseul@sbsd.net>
2025-01-03 18:26:00 -0800

    Hellooo

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,46 @@
 package _01_methods._1_houses;
+
+import java.awt.Color;
+import java.util.Random;
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
-		
+
+	Robot rob = new Robot();
+	
+	Random random = new Random();
+	
+
 	public void run() {
-		// Check the recipe to find out what code to put here
+		rob.setPenWidth(6);
+		rob.setSpeed(25);
+		rob.setX(75);
+		rob.setY(500);
+		rob.penDown();
+		for (int i = 1; i <= 10; i++) {
+			drawHouse(random.nextInt(350 - 100) + 100);
+			
+		}
+		
+		rob.hide();
+
+	}
+
+	private void drawHouse(int height) {
+		rob.move(height);
+		rob.turn(90);
+		rob.move(20);
+		rob.turn(90);
+		rob.move(height);
+		rob.setPenColor(new Color(3, 125, 6, 255));
+		rob.turn(-90);
+		rob.move(60);
+		rob.turn(-90);
+		rob.setPenColor(Color.black);
+
 	}
 }
