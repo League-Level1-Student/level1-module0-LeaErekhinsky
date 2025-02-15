7182daa413ce4303f4115c4849e11b3432a2348f
league <league@imac-22.attlocal.net>
2025-02-14 18:31:11 -0800

    Made significant progress on Flappy Kitty :)

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,10 +1,13 @@
 package _01_methods._5_FlappyBird;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class FlappyBird extends PApplet {
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
+    static final int WIDTH = 556;
+    static final int HEIGHT = 446;
+    PImage bowbg;
+    PImage kitty;
 
     @Override
     public void settings() {
@@ -13,11 +16,17 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void setup() {
+    	bowbg = loadImage("FlappyKittyGameBg.png");
+        bowbg.resize(WIDTH, HEIGHT);
+        kitty = loadImage("FlappyKittyGameKitty.png");
         
     }
 
     @Override
     public void draw() {
+    	background(bowbg);
+    	image(kitty, 0, 0);
+  
         
     }
 
