c45ce62106e88296ea0b775751c00b492b61a24f
league <league@imac-22.attlocal.net>
2025-02-21 18:29:05 -0800

    I changed my name to AAAAAAaaaAAaAaaaA 😊

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -4,10 +4,14 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class FlappyBird extends PApplet {
-    static final int WIDTH = 556;
-    static final int HEIGHT = 446;
+    static final int WIDTH = 1112;
+    static final int HEIGHT = 892;
     PImage bowbg;
     PImage kitty;
+    PImage temufactory;
+    int kittyy = 0;
+    int gravity = 1;
+    int kittyYVelocity = -50;
 
     @Override
     public void settings() {
@@ -19,17 +23,26 @@ public class FlappyBird extends PApplet {
     	bowbg = loadImage("FlappyKittyGameBg.png");
         bowbg.resize(WIDTH, HEIGHT);
         kitty = loadImage("FlappyKittyGameKitty.png");
+        temufactory = loadImage("temu-removebg-preview.png");
+        temufactory.resize(1112, 600);
         
     }
 
     @Override
     public void draw() {
     	background(bowbg);
-    	image(kitty, 0, 0);
-  
+    	image(temufactory, 0, HEIGHT - 485);
+    		image(kitty, 0, kittyy);
+    		kittyy += kittyYVelocity ;
+    		kittyYVelocity += gravity;
         
     }
 
+    public void mousePressed() {
+    	kittyYVelocity = -10;
+    	
+    }
+    
     static public void main(String[] args) {
         PApplet.main(FlappyBird.class.getName());
     }
